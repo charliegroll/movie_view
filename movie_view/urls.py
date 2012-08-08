a6fe72b1551d4e867989d605fc582d36ea755051
Charlie Groll <charliegroll@gmail.com>
2012-08-07 20:04:42 -0400

    Initial commit

---
+++ movie_view/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import patterns, include, url
+import views.*
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+        ('^home/', views.home),
+    # Examples:
+    # url(r'^$', 'movie_view.views.home', name='home'),
+    # url(r'^movie_view/', include('movie_view.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)

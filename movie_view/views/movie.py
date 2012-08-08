0d7bbc3a23f8b58e00fe1e6779925811b4f2f99f
Charlie Groll <charliegroll@gmail.com>
2012-08-07 23:19:47 -0400

    Logic work for API call

--- movie_view/views/movie.py
+++ movie_view/views/movie.py
@@ -13,12 +13,30 @@ def show(request, offset):
         raise Http404()
     else:
         t = get_template('movie.html')
-        html = t.render(Context({'movie': name}))
+        movie_id = process_movie(name)
+        html = t.render(Context({'movie': name, 'id': movie_id}))
         return HttpResponse(html)
 
 def parse_movie(name):
     name = name.split('-')
     if name == '':
-        return null
+        return None
     else:
         return " ".join(name).title()
+
+def process_movie(name):
+    tmdb.configure('36fb5f623484f4b2680f492005762f31') #store this key somewhere
+    # something's going wrong here
+    movie = tmdb.Movie(name)
+    try:
+        if None == movie:
+            return None
+
+        movie.get_id()
+        movie.full_info(movie_id)
+
+        return movie_id
+    except:
+        return None
+
+

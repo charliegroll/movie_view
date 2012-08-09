480116ba1cb131fe939fb6363cf9d32ede0253b0
Charlie Groll <charliegroll@gmail.com>
2012-08-08 21:04:46 -0400

    Testing for new API

--- movie_view/views/movie.py
+++ movie_view/views/movie.py
@@ -1,11 +1,12 @@
-import tmdb # make sure to install pip and install requests
+# import tmdb # make sure to install pip and install requests
+from tmdb3 import set_key, set_locale, searchMovie
 from django.http import HttpResponse, Http404
 from django.template.loader import get_template
 from django.template import Context
 import string
 
-def show(request, offset):
-    name = parse_movie(offset)
+def show(request, name):
+    name = parse_movie(name)
 
     ua = request.META.get('HTTP_USER_AGENT', 'unknown')
 
@@ -25,17 +26,14 @@ def parse_movie(name):
         return " ".join(name).title()
 
 def process_movie(name):
-    tmdb.configure('36fb5f623484f4b2680f492005762f31') #store this key somewhere
-    # something's going wrong here
-    movie = tmdb.Movie(name)
+    set_key('36fb5f623484f4b2680f492005762f31') #store this key somewhere
+    set_locale()
     try:
+        movie = searchMovie(name)
         if None == movie:
             return None
 
-        movie.get_id()
-        movie.full_info(movie_id)
-
-        return movie_id
+        return movie[0]
     except:
         return None
 

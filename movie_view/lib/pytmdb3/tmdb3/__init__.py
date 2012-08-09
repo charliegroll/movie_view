3c71fc5aa957346218808448432fac2fe42e91ae
Charlie Groll <charliegroll@gmail.com>
2012-08-08 21:03:56 -0400

    Changed APIs to pytmdb3... and it works!

---
+++ movie_view/lib/pytmdb3/tmdb3/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from tmdb_api import Configuration, searchMovie, searchMovieWithYear, \
+                     searchPerson, searchStudio, Studio, Person, Movie, \
+                     Collection, Genre, __version__
+from request import set_key, set_cache
+from locales import get_locale, set_locale
+from tmdb_auth import get_session, set_session
+from cache_engine import CacheEngine
+from tmdb_exceptions import *
+

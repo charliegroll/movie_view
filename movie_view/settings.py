a64ebe711139442dfbe42bbf1fccbe718e5e5636
Charlie Groll <charliegroll@gmail.com>
2012-08-09 22:06:49 -0400

    Still trying for static files...

--- movie_view/settings.py
+++ movie_view/settings.py
@@ -61,15 +61,13 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/lib/'
+STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-
-    '/lib/rawr/lib/',
 )
 
 # List of finder classes that know how to find static files in

5829ade09201b1c08066a681b87fe55ce625be73
Charlie Groll <charliegroll@gmail.com>
2012-09-13 22:09:46 -0400

    Got search working!!! Problem is, the DOM is generated by jQuery

--- movie_view/static/js/scripts.js
+++ movie_view/static/js/scripts.js
@@ -1,6 +1,9 @@
-var imdbids = []
+var imdbids = [];
+var ALL;
 
 $(document).ready(function(){
+    ALL = $('.movie-item');
+    wrapyourtool();
     $('#movie-grid *, header, footer').hide().load().delay(1000).fadeIn(800);
 
     $('.movie-item').hover(function() {
@@ -14,4 +17,25 @@ $(document).ready(function(){
     }).mouseup(function() {
         $(this).css('background-color', '');
     });
+
+    $('.search-query').on('keyup', function() {
+        var key = $(this).val().toLowerCase();
+        $('#movie-grid').empty();
+        ALL.each(function() {
+            var text = $(this).find('p').text().toLowerCase();
+            if (text.indexOf(key) >= 0) {
+                console.log($(this));
+                $('#movie-grid').append($(this));
+            }
+        });
+        wrapyourtool();
+    });
 });
+
+function wrapyourtool() {
+    var wrapper = '<div class="movie-row row-fluid"></div>';
+    $('.movie-item:nth-child(6n)').each(function() {
+        $(this).prevAll('.movie-item').andSelf().wrapAll(wrapper);
+    });
+    $('#movie-grid>.movie-item').wrapAll(wrapper);
+}

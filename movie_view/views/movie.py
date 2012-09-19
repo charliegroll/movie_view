7d4fd5c17ac97f0253576fced12efd7ee7f28a24
Charlie Groll <charliegroll@gmail.com>
2012-09-18 23:04:35 -0400

    Pickling!

--- movie_view/views/movie.py
+++ movie_view/views/movie.py
@@ -6,11 +6,19 @@ from django.template.loader import get_template
 import string
 import bisect
 import operator
+from os.path import expanduser as expand
+
+try:
+    import cPickle as pickle
+except:
+    import pickle
 
 from filesystem import explore, fileset, MovieFile
 
 movieids = list()
 moviesToDisplay = list()
+
+datafile = '/.movie.info'
 
 def showall(request):
     t = get_template('home.html')
@@ -25,7 +33,10 @@ def process_movies(dir):
     set_key('36fb5f623484f4b2680f492005762f31') #store this key somewhere
     set_locale()
 
+    dir = expand(dir)
+
     movies = list()
+    moviesToDisplay = loadfromfile(dir)
     explore(dir)
 
     for f in fileset:
@@ -37,6 +48,9 @@ def process_movies(dir):
 
     for m in movies:
         if not m:
+            continue
+
+        if m.id in movieids:
             continue
 
         p = m.poster
@@ -54,7 +68,24 @@ def process_movies(dir):
 
         moviesToDisplay.append(d)
 
+    writetofile(dir, moviesToDisplay)
     return sorted(moviesToDisplay, key=lambda x: x.movie.title)
+
+def loadfromfile(dir):
+    f = open(dir + datafile, 'r')
+    mtds = pickle.load(f)
+
+    for mtd in mtds:
+        pos = bisect.bisect(movieids, mtd.movie.id)
+        movieids.insert(pos, mtd.movie.id)
+
+    f.close()
+    return mtds
+
+def writetofile(dir, mtd):
+    f = open(dir + datafile, 'w')
+    pickle.dump(mtd, f, -1)
+    f.close()
 
 class DisplayMovie:
     def __init__(self, movie, poster):
@@ -63,4 +94,4 @@ class DisplayMovie:
         self.poster = poster
         self.year = (movie.releasedate.year if movie.releasedate else 0)
     def __str__(self):
-        return repr(self.movie.title + ' (' + self.year + ')')
+        return repr(self.movie.title + ' (' + str(self.year) + ')')
